5a09bd54a2eee74daf522c1ad070a4ba7aac6f09
John Patrick Salangsang <salangsangjpmoreno@gmail.com>
2025-03-31 14:21:44 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Inventory; // Import Inventory model
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        $faker = Faker::create();
+
+        // Generate 20 fake inventory items
+        for ($i = 0; $i < 20; $i++) {
+            Inventory::create([
+                'brand_name' => $faker->company,
+                'type' => $faker->randomElement(['Laptop', 'Monitor', 'Keyboard', 'Mouse', 'Printer']),
+                'quantity_on_hand' => $faker->numberBetween(5, 100),
+                'price' => $faker->randomFloat(2, 500, 50000), // Price between 500 and 50,000
+                'products_sold' => $faker->numberBetween(0, 50),
+            ]);
+        }
     }
 }
