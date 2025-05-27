cede8d77a34e51ddfa07ec6418a8ea1b6fcfe4b2
John Patrick Salangsang <salangsangjpmoreno@gmail.com>
2025-05-27 20:00:29 +0800

    Successfully created a signup and login feature

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -25,7 +25,7 @@ class AuthController extends Controller
         $user = User::create([
             'name'     => $request->name,
             'email'    => $request->email,
-            'password' => Hash::make($request->password),
+            'password' => $request->password,
         ]);
 
         $token = JWTAuth::fromUser($user);
--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Contracts\Auth\Factory as Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Throwable;
 
 class Authenticate
 {
